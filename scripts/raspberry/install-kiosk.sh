21ed8180ea5e49e1b06003dd33e375a1551444e3
martin <martin.stusek@gmail.com>
2022-01-27 17:40:49 +0100

    test

--- scripts/raspberry/install-kiosk.sh
+++ scripts/raspberry/install-kiosk.sh
@@ -9,8 +9,34 @@ sudo apt-get install --no-install-recommends cec-utils
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
 #Install chromium
 sudo apt-get install --no-install-recommends chromium-browser
+read -e -p "Do you want to install file server? (Options: y|Y, n,N) " choice
+case ${choice:0:1} in
+    y|Y )
+        HOSTNAME=$(hostname)
+        if [ "$HOSTNAME" == "zeroconf" ]; then
+            sudo ./install.sh
+            echo "Proceeding with file server installation!"
+        else 
+            read -e -p "The hostname is not set to zeroconf. Please input new hostname or y|Y to keep the default (fileserver): (Options: y|Y, <hostname>) " choice
+            case ${choice:0:1} in
+                y|Y )
+                    echo "Setting hostname to default (fileserver)!"
+                    sudo raspi-config nonint do_hostname fileserver
+                ;;
+                * )
+                    echo "Setting hostname to $(choice)!"
+                    sudo raspi-config nonint do_hostname $(choice)
+                ;;
+            esac
+        fi
+    ;;
+    n|N ) 
+       echo "Skipping file server installation."
+    ;;
+esac
 #Update autostart file
 sudo chmod 777 /etc/xdg/openbox/autostart
+head -n -7 /etc/xdg/openbox/autostart > /etc/xdg/openbox/autostart
 sudo echo 'xset -dpms            # turn off display power management system' >> /etc/xdg/openbox/autostart
 sudo echo 'xset s noblank        # turn off screen blanking' >> /etc/xdg/openbox/autostart
 sudo echo 'xset s off            # turn off screen saver' >> /etc/xdg/openbox/autostart
@@ -18,7 +44,7 @@ sudo echo $'sed -i \'s/"exited_cleanly":false/"exited_cleanly":true/\' ~/.config
 sudo echo $'sed -i \'s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/\' ~/.config/chromium/Default/Preferences' >> /etc/xdg/openbox/autostart
 sudo echo 'chromium-browser  --noerrdialogs --disable-infobars --kiosk $KIOSK_URL' >> /etc/xdg/openbox/autostart
 sudo echo '--check-for-update-interval=31536000' >> /etc/xdg/openbox/autostart
-sudo chmod 755 /etc/xdg/openbox/environment
+sudo chmod 755 /etc/xdg/openbox/autostart
 #Add KIOSK_URL to the openbox environment
 sudo chmod 777 /etc/xdg/openbox/environment
 read -e -p "Do you want to change default kiosk URL? (Options: y|Y, <address>) " choice
@@ -29,11 +55,11 @@ case ${choice:0:1} in
     ;;
     * ) 
        echo "Setting $choice as default kiosk URL."
-       sudo echo 'export KIOSK_URL=$choice' >> /etc/xdg/openbox/environment
+       sudo echo "export KIOSK_URL=$choice" >> /etc/xdg/openbox/environment
     ;;
 esac
 
-sudo chmod 755 /etc/xdg/openbox/autostart
+sudo chmod 755 /etc/xdg/openbox/environment
 #Make sure the xserver start on boot
 FILE=/home/pi/.bash_profile
 if [ -f "$FILE" ]; then
@@ -123,16 +149,16 @@ sudo chmod 775 $START
 fi
 (crontab -l 2>/dev/null; echo "@reboot /home/pi/start.sh") | crontab -
 
-read -e -p "Do you want to install File Server? (Options: y|Y, n|N) " choice
-case ${choice:0:1} in
-    y|Y )
-        echo "Proceeding with File Server installation"
-        sudo ./install.sh
-    ;;
-    n|N ) 
-       echo "Skipping File Server installation!"
-    ;;
-esac
+#read -e -p "Do you want to install File Server? (Options: y|Y, n|N) " choice
+#case ${choice:0:1} in
+#    y|Y )
+#        echo "Proceeding with File Server installation"
+#        sudo ./install.sh
+#    ;;
+#    n|N ) 
+#       echo "Skipping File Server installation!"
+#    ;;
+#esac
 
 sudo apt-get install --no-install-recommends realvnc-vnc-server 
 sudo raspi-config nonint do_vnc 0
